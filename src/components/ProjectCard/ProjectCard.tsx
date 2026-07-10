d0d4b1e7e0f560680987cbfd4e00f642c5810e2d
Ana <anasiqueira.serpentis@gmail.com>
2026-07-10 20:59:00 -0300

    fix: adjust image alignment and sizing in ProjectCard component

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -11,8 +11,8 @@ export default function ProjectCard({ project, index }: ProjectCardProps) {
     const isEven = index % 2 === 0;
 
     return (
-        <div className={`w-full flex flex-col md:flex-row items-center gap-8 lg:gap-12 py-16 px-4 md:py-12 lg:py-20 1720:px-16 justify-between xl:justify-center font-sans text-xs lg:text-sm font-light text-foreground ${ isEven ? "md:pl-8 md:pr-0 xl:pl-16" : "md:flex-row-reverse md:pr-8 md:pl-0 xl:pr-16" }`}>
-            <div className="flex flex-col gap-3 lg:gap-5 md:max-w-sm lg:max-w-xl xl:max-w-lg 2xl:max-w-lg">
+        <div className={`w-full flex flex-col md:flex-row items-center gap-8 lg:gap-12 py-16 px-4 md:py-12 lg:py-20 1720:px-16 justify-between xl:justify-center font-sans text-xs lg:text-sm font-light text-foreground ${ isEven ? "md:pl-8 md:pr-0 xl:pl-16" : "md:flex-row-reverse md:pr-8 md:pl-0 md:gap-2 xl:pr-16" }`}>
+            <div className="flex flex-col gap-3 lg:gap-5 md:max-w-sm lg:max-w-md xl:max-w-lg 2xl:max-w-lg">
                 <div className="project-heading">
                     <div className="flex item justify-start gap-2 md:gap-4 mb-4">
                         { project.tags.map((tag, index) => (
@@ -62,13 +62,13 @@ export default function ProjectCard({ project, index }: ProjectCardProps) {
                     <p>{project.handoff?.content}</p>
                 </div>
             </div>
-            <div className="project-image overflow-hidden">
+            <div className={`project-image overflow-hidden flex ${isEven ? "justify-start" : "justify-end"}`}>
                 <Image
                     src={project.image}
                     alt={project.title}
                     width={840}
                     height={800}
-                    className={isEven ? "max-w-none w-110 sm:w-150 md:w-165 lg:w-200 xl:w-225 2xl:w-300" : "max-w-none w-110 ml-1 sm:w-150 md:w-150 md:-translate-x-19"}
+                    className={isEven ? "max-w-none w-110 sm:w-150 md:w-165 lg:w-200 xl:w-225 2xl:w-300" : "max-w-none w-110 sm:w-150 md:w-140 lg:w-180 xl:w-200 2xl:w-270"}
                 />
             </div>
         </div>
