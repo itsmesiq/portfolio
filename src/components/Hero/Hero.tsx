a6774353c8adbbb30ddd4b2c249c47dbc89e186b
Ana <anasiqueira.serpentis@gmail.com>
2026-07-10 22:54:27 -0300

    feat: add animations to Hero component elements

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import Button from "@/components/ui/Button";
 import Image from "next/image";
 import { socialMedia } from "@/data/social";
 import * as motion from "motion/react-client";
+import { easeIn, stagger } from "motion";
 
 const techStack = [
     {
@@ -37,6 +38,28 @@ const techStack = [
         image: '/img/hero/hero-icons/github.svg'
     },
 ];
+
+const containerVariants = {
+    hidden: {},
+    visible: {
+        transition: {
+            duration: 0.7,
+            delayChildren: 0.2,
+            staggerChildren: 0.15
+        }
+    }
+};
+
+const itemVariants = {
+    hidden: {
+        opacity: 0,
+        y: 20
+    },
+    visible: {
+        opacity: 1,
+        y: 0
+    }
+};
 
 export default function Hero() {
     return (
@@ -75,19 +98,49 @@ export default function Hero() {
                 </div>
             </div>
             <div className="flex flex-col gap-8 mt-8 lg:max-w-[1560px] items-center mx-auto">
-                <Image
-                    src="/img/hero/profile.png"
-                    alt="Profile Image"
-                    width={216}
-                    height={216}
-                    className="w-45 h-45 md:w-54 md:h-54"
-                />
-                <div className="text-center">
-                    <p className="text-2xl lg:text-4xl font-light">Ana Siqueira</p>
-                    <h1 className="font-heading text-3xl lg:text-5xl leading-[normal] text-foreground font-bold">UI Designer & Frontend Developer</h1>
-                    <p className="text-base lg:text-xl font-light max-w-270 mt-3">Crio interfaces digitais de alta performance para e-commerce, combinando design estratégico, responsividade e foco em conversão. Habilidade em Frontend para garantir uma implementação fiel ao projeto.</p>
-                </div>
-                <ul className="flex gap-4 items-center justify-center my-4 flex-wrap">
+                <motion.div
+                    initial={{
+                        opacity: 0,
+                        scale: 0.5
+                    }}
+                    animate={{
+                        opacity: 1,
+                        scale: 1
+                    }}
+                    transition={{
+                        duration: 0.3,
+                        ease: "easeIn"
+                    }}
+                >
+                    <Image
+                        src="/img/hero/profile.png"
+                        alt="Profile Image"
+                        width={216}
+                        height={216}
+                        className="w-45 h-45 md:w-54 md:h-54"
+                    />
+                </motion.div>
+                <motion.div className="text-center" variants={containerVariants} initial="hidden" animate="visible">
+                    <motion.p className="text-2xl lg:text-4xl font-light" variants={itemVariants} transition={{ease: "easeIn"}}>Ana Siqueira</motion.p>
+                    <motion.h1 className="font-heading text-3xl lg:text-5xl leading-[normal] text-foreground font-bold" variants={itemVariants} transition={{ease: "easeIn"}}>UI Designer & Frontend Developer</motion.h1>
+                    <motion.p className="text-base lg:text-xl font-light max-w-270 mt-3" variants={itemVariants} transition={{ease: "easeIn"}}>Crio interfaces digitais de alta performance para e-commerce, combinando design estratégico, responsividade e foco em conversão. Habilidade em Frontend para garantir uma implementação fiel ao projeto.</motion.p>
+                </motion.div>
+                <motion.ul 
+                    className="flex gap-4 items-center justify-center my-4 flex-wrap"
+                    initial={{
+                        opacity: 0,
+                        y: 20
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: 0
+                    }}
+                    transition={{
+                        delay: 0.7,
+                        duration: 0.6,
+                        ease: "easeIn"
+                    }}
+                >
                     {techStack.map((item, index) => (
                         <li key={index} className="flex items-center gap-2 px-4 py-2 bg-surface-gray rounded-4xl font-sans text-text-gray-200 text-sm sm:text-base leading-5 font-normal text-center">
                             <Image
@@ -100,7 +153,7 @@ export default function Hero() {
                             {item.label}
                         </li>
                     ))}
-                </ul>
+                </motion.ul>
 
             </div>
             <motion.div 
