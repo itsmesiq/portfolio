a173acb97c32bdf9047f409fc71e10e0169c9137
Ana <anasiqueira.serpentis@gmail.com>
2026-07-15 20:41:18 -0300

    feat: add Header component with navigation, logo, and theme toggle

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Button from "../ui/Button";
+import { ThemeToggle } from "../Theme/ThemeToggle";
+import { Logo } from "../ui/Logo";
+import { LanguageToggle } from "../ui/LanguageToggle";
+
+export default function Header() {
+    return (
+        <header className="w-full items-center flex justify-between px-16 bg-background font-heading text-base py-6 font-normal">
+            <nav>
+                <ul className="flex space-x-6">
+                    <li><a href="#" className="text-foreground hover:text-primary transition-colors duration-500">Sobre Mim</a></li>
+                    <li><a href="#" className="text-foreground hover:text-primary transition-colors duration-500">Cases</a></li>
+                    <li><a href="#" className="text-foreground hover:text-primary transition-colors duration-500">Skills</a></li>
+                    <li><a href="#" className="text-foreground hover:text-primary transition-colors duration-500">Contato</a></li>
+                </ul>
+            </nav>
+            <Logo />
+            <div className="flex items-center gap-5">
+                <LanguageToggle />
+                <ThemeToggle />
+                <Button href="/cv/curriculoAnaSiqueira.pdf">
+                    Baixar Currículo
+                    <Image
+                        src="/img/icon/download.svg"
+                        alt="Download Icon"
+                        width={24}
+                        height={24}
+                    />
+                </Button>
+            </div>
+        </header>
+    );
+}
