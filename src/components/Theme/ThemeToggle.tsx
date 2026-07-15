6c0a52cbab1e65659725629c7c0b81b55b27d6e4
Ana <anasiqueira.serpentis@gmail.com>
2026-07-15 20:39:57 -0300

    fix: update theme toggle to use resolvedTheme for improved functionality

--- src/components/Theme/ThemeToggle.tsx
+++ src/components/Theme/ThemeToggle.tsx
@@ -4,14 +4,19 @@ import Image from "next/image";
 import { useTheme } from "next-themes";
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
+
+  if (!resolvedTheme) {
+    return null;
+  }
 
   return (
     <button
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+        className="flex items-center justify-center rounded-lg border border-border w-10 h-10 hover:bg-secondary-hover transition-colors duration-500"
     >
         <Image
-            src={theme === "dark" ? "/img/icon/sun.svg" : "/img/icon/moon.svg"}
+            src={resolvedTheme === "dark" ? "/img/icon/sun.svg" : "/img/icon/moon.svg"}
             alt="Toggle theme"
             width={24}
             height={24}
