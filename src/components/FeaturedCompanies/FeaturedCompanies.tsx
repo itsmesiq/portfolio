6cec6e8249ccb0b644917d699cf339bd2acb1043
Ana <anasiqueira.serpentis@gmail.com>
2026-07-16 13:07:07 -0300

    feat: add Casa Almeida to Featured Companies section

--- src/components/FeaturedCompanies/FeaturedCompanies.tsx
+++ src/components/FeaturedCompanies/FeaturedCompanies.tsx
@@ -22,12 +22,19 @@ const companies = [
         height: 22,
         className: 'max-w-14.25 max-h-4 sm:max-w-20 sm:max-h-5.5 opacity-60 hover:opacity-100 transition-opacity duration-300'
     },
+    { 
+        label: 'Casa Almeida',
+        image: '/img/featured/casaalmeida.svg',
+        width: 150,
+        height: 15,
+        className: 'max-w-30.5 max-h-3.5 sm:max-w-37.5 sm:max-h-4.25 opacity-60 hover:opacity-100 transition-opacity duration-300'
+    }
 ];
 
 export default function FeaturedCompanies() {
     return (
-        <div className="w-full px-6 sm:px-16 py-6 flex items-center justify-between md:justify-center sm:gap-8 lg:gap-20 bg-surface-gray text-text-gray-200 font-sans text-sm lg:text-lg xl:text-xl font-light">
-            <span className="hidden md:inline">Projetos em Destaque:</span>
+        <div className="w-full px-6 sm:px-16 py-6 flex items-center justify-between md:justify-center sm:gap-8 lg:gap-20 bg-card">
+            <span className="hidden md:inline text-muted font-sans text-sm lg:text-lg xl:text-xl font-light">Projetos em Destaque:</span>
             {companies.map((company, index) => (
                 <Image
                     key={index}
