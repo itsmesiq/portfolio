f7c42a0b69d69d42d74fb5befa57b3cb8a52e965
Ana <anasiqueira.serpentis@gmail.com>
2026-07-18 13:34:58 -0300

    feat: update layout in FeaturedCompanies component for improved responsiveness

--- src/components/FeaturedCompanies/FeaturedCompanies.tsx
+++ src/components/FeaturedCompanies/FeaturedCompanies.tsx
@@ -2,8 +2,8 @@ import { featuredCompanies } from "@/data/projects";
 
 export default function FeaturedCompanies() {
     return (
-        <div className="w-full px-6 sm:px-16 py-6 flex items-center justify-between md:justify-center sm:gap-8 lg:gap-20 bg-card">
-            <span className="hidden md:inline text-muted font-sans text-sm lg:text-lg xl:text-xl font-light">Projetos em Destaque:</span>
+        <div className="w-full px-6 md:px-16 py-6 flex flex-wrap items-center justify-center gap-6 sm:gap-8 xl:gap-20 bg-card">
+            <span className="hidden lg:inline text-muted font-sans text-sm lg:text-lg xl:text-xl font-light">Projetos em Destaque:</span>
             {featuredCompanies.map((company, index) => (
                 company.image && (
                     <company.image key={index} className={company.className} />
