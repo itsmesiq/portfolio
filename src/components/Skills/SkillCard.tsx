8e0640306a23554a7a9bd68b310b2748b1b6e0b6
Ana <anasiqueira.serpentis@gmail.com>
2026-07-18 22:02:38 -0300

    feat: implement carousel functionality in Skills component and enhance layout for better presentation

--- src/components/Skills/SkillCard.tsx
+++ src/components/Skills/SkillCard.tsx
@@ -6,7 +6,7 @@ type SkillCardProps = {
 
 export default function SkillCard({ skill }: SkillCardProps) {
     return (
-        <div className="flex flex-col items-start self-stretch gap-3 py-8 px-8 bg-card rounded-3xl max-w-72.25 w-full overflow-visible shadow-none hover:shadow-[0_0_20px_rgba(254,182,0,0.60)] transition-all transition-discrete ease-in-out duration-500">
+        <div className="flex flex-col items-start self-stretch gap-3 py-8 px-8 bg-card rounded-3xl w-72.25 overflow-visible shadow-none hover:shadow-[0_0_20px_rgba(254,182,0,0.60)] transition-all transition-discrete ease-in-out duration-500">
             {skill.icon && <skill.icon className="size-14" />}
             <h3 className="font-heading text-xl text-foreground font-semibold">{skill.title}</h3>
             <p className="font-sans text-sm font-normal text-muted ">{skill.description}</p>
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,17 +1,54 @@
+'use client';
+
 import SkillCard from "./SkillCard";
 import { skills } from "@/data/skills";
+import useEmblaCarousel from "embla-carousel-react";
+import { CarouselIndicators } from "../Cases/CaseCarousel/CarouselIndicators";
+import { useEffect, useState } from "react";
 
 export default function Skills() {
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        loop: false,
+        align: "center",
+        active: true,
+        breakpoints: {
+            '(min-width: 1280px)': { active: false },
+        },
+    });
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        if (!emblaApi) return;
+
+        const onSelect = () => {
+            setCurrentIndex(emblaApi.selectedScrollSnap());
+        };
+
+        emblaApi.on("select", onSelect);
+
+        onSelect();
+
+        return () => {
+            emblaApi.off("select", onSelect);
+        };
+    }, [emblaApi]);
+
     return (
-        <section id="skills" className="w-full px-16 my-22">
-            <div className="font-heading mb-8 flex flex-col items-center gap-0">
+        <section id="skills" className="w-full my-22">
+            <div className="font-heading mb-6 flex flex-col items-center gap-0">
                 <span className="text-xl font-normal text-accent">Expertise</span>
-                <h2 className="text-3xl font-semibold text-foreground">Design & Desenvolvimento</h2>
+                <h2 className="text-3xl font-semibold text-foreground text-center">Design & Desenvolvimento</h2>
             </div>
-            <div className="flex items-start justify-between 2xl:justify-center 2xl:gap-10">
-                {skills.map((skill) => (
-                    <SkillCard key={skill.label} skill={skill} />
-                ))}
+            <div ref={emblaRef} className="overflow-hidden py-5 px-5 sm:px-10 lg:px-16">
+                <div className="grid grid-flow-col auto-cols-max gap-8 xl:gap-10 xl:justify-center">
+                    {skills.map((skill) => (
+                        <SkillCard key={skill.label} skill={skill} />
+                    ))}
+                </div>
+            </div>
+            <div className="flex justify-center mt-2 xl:hidden">
+                <CarouselIndicators total={skills.length} currentIndex={currentIndex} onSelect={setCurrentIndex} />
             </div>
         </section>
     );
