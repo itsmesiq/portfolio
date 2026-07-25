1451a72d640a71c9f5451c5140d64f81fdeda2c8
Ana <anasiqueira.serpentis@gmail.com>
2026-07-25 19:12:29 -0300

    feat: Hero Case responsive

--- src/components/CasesPage/Hero.tsx
+++ src/components/CasesPage/Hero.tsx
@@ -8,14 +8,14 @@ type CaseProps = {
 
 export default function HeroCases({ project }: CaseProps) {    
     return (
-        <section id="hero" className="w-full mt-22 px-4 sm:px-10 md:px-1 lg:px-16">
-            <div className="flex justify-end mt-5">
+        <section id="hero" className="w-full mt-22 px-4 lg:px-10 xl:px-16">
+            <div className="flex justify-end mt-0 lg:mt-5">
                 <BackBtn />
             </div>
-            <div className="flex items-center py-16 justify-between 2xl:justify-center 2xl:gap-16">
-                <div className="pb-8 flex flex-col gap-8">
+            <div className="flex items-center flex-col-reverse py-2 gap-6 lg:flex-row lg:justify-between lg:py-5 lg:gap-0 xl:py-16 2xl:justify-center 2xl:gap-16">
+                <div className="pb-8 sm:px-10 flex flex-col gap-8 lg:px-0">
                     <div className="flex flex-col gap-4">
-                        <div className="flex justify-start gap-3">
+                        <div className="flex justify-start gap-2 flex-wrap sm:gap-3">
                             {project.tags.map((tag, tagIndex) => (
                                 <div key={tagIndex}>
                                     <span className="inline-block bg-foreground text-background text-xs font-normal px-3 py-1 rounded-full">
@@ -23,27 +23,30 @@ export default function HeroCases({ project }: CaseProps) {
                                     </span>
                                 </div>
                             ))}
-                        </div>                        
-                        <project.logo className="w-104.5 h-12" />
-                        <div className="flex gap-3 font-heading text-foreground text-sm font-light max-w-104.5">
-                            <div className="w-0.5 bg-primary"></div>
-                            <p>{project.subtitle}</p>
+                        </div> 
+                        <div className="flex flex-col gap-6">                       
+                            <project.logo.svg className={project.logo.size} />
+                            <div className="flex gap-3 font-heading text-foreground text-sm font-light max-w-104.5">
+                                <div className={'w-1'} style={{ backgroundColor: project.color }}></div>
+                                <p>{project.subtitle}</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-14 font-sans max-w-112.75">
+                    <div className="flex flex-col gap-8 font-sans sm:gap-14 md:max-w-191 lg:max-w-112.75">
                         <p className="text-sm text-muted">{project.description}</p>
                         <span className="text-xs font-medium text-foreground">{project.credits}</span>
                     </div>
                 </div>
-                <Image
-                    src={project.preview}
-                    alt={project.title}
-                    width={1500}
-                    height={841}
-                    loading="eager"
-                    className="h-auto w-206 2xl:w-250 2xl:h-auto object-cover"
-                />
-
+                <div className="overflow-hidden flex justify-center -ml-20 sm:ml-0 lg:justify-start lg:-mr-10 xl:mr-0">
+                    <Image
+                        src={project.preview}
+                        alt={project.title}
+                        width={1500}
+                        height={841}
+                        loading="eager"
+                        className="h-auto w-120 max-w-none sm:w-150 sm:h-auto md:w-180 md:h-auto xl:w-250 xl:h-auto object-cover"
+                    />
+                </div> 
             </div>
         </section>
     );
